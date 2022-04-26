083f40466b502723bb38cd22801e467a0d42172b
Command <maoxs2@gmail.com>
2022-04-26 11:24:31 +0800

    show creator && date on feeds

--- src/pages/FeedsPage.vue
+++ src/pages/FeedsPage.vue
@@ -24,7 +24,7 @@
           <!-- <v-img height="250" :src="imgs[index]"></v-img> -->
 
           <v-card-title>{{ item.title }}</v-card-title>
-
+          <v-card-subtitle>{{ item.creator }} on {{ Date(item.isoDate) }}  </v-card-subtitle>
           <v-card-text>
             <div class="my-4 grey--text" v-html="item.contentSnippet"></div>
           </v-card-text>
@@ -48,7 +48,7 @@
             <v-btn
               color="deep-purple lighten-2 right"
               text
-              :href="item.link"
+              :href="item['link']"
               target="_blank"
             >
               READ MORE
@@ -105,15 +105,22 @@ export default {
   mounted() {
     this.page = parseInt(this.$route.query.page) || 1;
     this.axios
-      .get(`https://www.saineating.ngx.fi/api/feeds`)
+      .get("https://www.saineating.ngx.fi/api/feeds/count")
+      .then((response) => {
+        console.log(response);
+        this.pages = (1 + response.data.count / this.count) >> 0;
+      });
+        const offset = (this.page - 1) * 12;
+    this.axios
+      .get(`https://www.saineating.ngx.fi/api/feeds?offset=${offset}&count=${this.count}`)
       .then((response) => {
         console.log(response);
         this.items = response.data.items;
-        let imgs = {};
-        Object.values(this.items).forEach((item, index) => {
-          imgs[index] = response.data.image.link;
-        });
-        this.imgs = imgs;
+        // let imgs = {};
+        // Object.values(this.items).forEach((item, index) => {
+        //   imgs[index] = response.data.image.link; // invalid due to src CORS
+        // });
+        // this.imgs = imgs;
       });
   },
   methods: {
@@ -121,7 +128,7 @@ export default {
       this.$set(this.show, index, !this.show[index]);
     },
     next_page(page) {
-      this.$router.push({ path: "/", query: { page: page } });
+      this.$router.push({ path: "/feeds/", query: { page: page } });
     },
   },
 };
